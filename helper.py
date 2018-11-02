571a04d655b31cd30a4bf05ea7570ca063b2f533
georgeliu1998 <georgeliu1998@gmail.com>
2018-11-02 16:17:46 -0400

    Separate load data and plot wordcloud functions into new script

---
+++ helper.py
@@ -0,0 +1,54 @@
+import json
+from wordcloud import WordCloud, STOPWORDS
+from matplotlib import pyplot as plt
+
+
+def load_data(file_name):
+    """
+    Open the saved json data file and load the data into a dict.
+    
+    Parameters:
+        file_name: the saved file name, e.g. "machine_learning_engineer.json"
+    
+    Returns:
+        postings_dict: data in dict format   
+    
+    """
+
+    with open(file_name, 'r') as f:
+        postings_dict = json.load(f)
+        return postings_dict
+
+
+
+def plot_wc(text, max_words=200, stopwords_list=[], to_file_name=None):
+    """
+    Make a word cloud plot using the given text.
+    
+    Parameters:
+        text -- the text as a string
+    
+    Returns:
+        None    
+    """
+    wordcloud = WordCloud().generate(text)
+    stopwords = set(STOPWORDS)
+    stopwords.update(stopwords_list)
+
+    wordcloud = WordCloud(background_color='white',
+                         stopwords=stopwords,
+                         #prefer_horizontal=1,
+                         max_words=max_words, 
+                         min_font_size=6,
+                         scale=1,
+                         width = 800, height = 800, 
+                         random_state=8).generate(text)
+    
+    plt.figure(figsize=[16,16])
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+    
+    if to_file_name:
+        to_file_name = to_file_name + ".png"
+        wordcloud.to_file(to_file_name)
